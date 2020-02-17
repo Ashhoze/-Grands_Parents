99164a16caf85300f0261a3c0e0986b78d7402ed
Ashley Hozé <ashleyhoze@hotmail.fr>
2020-02-17 17:01:46 +0100

    generate routes and controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'grands_parents#index'
+  root to: 'grand_parents#index'
+  resources :grand_parents do
+    resources :bookings, only: [:new, :create]
+  end
+  resources :bookings, only: [:destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
