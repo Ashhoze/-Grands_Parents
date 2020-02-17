6657e2a4440c4ce1c6575ef8c53dee9dd02f8ad9
Ashley Hozé <ashleyhoze@hotmail.fr>
2020-02-17 16:48:20 +0100

    valisates in models

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,5 @@
 class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :grand_parent
+  validates :date_start, :date_end, presence: true
 end
--- app/models/grand_parent.rb
+++ app/models/grand_parent.rb
@@ -1,4 +1,5 @@
 class GrandParent < ApplicationRecord
   has_many :bookings
   belongs_to :user
+  validates :first_name, :last_name, :price, :city, :description, presence: true
 end
