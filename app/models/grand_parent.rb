9f16410823b34ca1ed26bff6b43cc5eb7670e63d
Ashley Hozé <ashleyhoze@hotmail.fr>
2020-02-18 11:15:07 +0100

    cloundinry

--- app/models/grand_parent.rb
+++ app/models/grand_parent.rb
@@ -1,4 +1,5 @@
 class GrandParent < ApplicationRecord
+  has_many_attached :photos
   has_many :bookings
   belongs_to :user
   validates :first_name, :last_name, :price, :city, :description, presence: true
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  has_one_attached :avatar
   has_many :bookings
   has_many :booked_grand_parents, through: :bookings, source: :grand_parent
   has_many :grand_parents
