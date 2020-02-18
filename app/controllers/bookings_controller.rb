b8134cea88a3344cd8284cb195e1ec8bd04dfa96
Ashley Hozé <ashleyhoze@hotmail.fr>
2020-02-18 14:14:11 +0100

    logo + new grandparents

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,28 @@
 class BookingsController < ApplicationController
+    def new
+    @grand_parent = GrandParent.find(params[:grand_parent_id])
+    @booking = Booking.new
+  end
+  def create
+    @booking = Booking.new(booking_params)
+    @grand_parent = GrandParent.find(params[:grand_parent_id])
+    @booking.grand_parent = @grand_parent
+    if @booking.save
+      redirect_to grand_parent_path(@booking.grand_parent)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @booking = Booking.find(params[:id])
+    @booking.destroy
+    redirect_to grand_parent_path(@booking.grand_parent)
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:date_start, :date_end, :review, :user_id, :grand_parent_id, :total_price)
+  end
 end
--- app/controllers/grand_parents_controller.rb
+++ app/controllers/grand_parents_controller.rb
@@ -1,6 +1,33 @@
 class GrandParentsController < ApplicationController
-  skip_before_action :authenticate_user!, only: :index
+  skip_before_action :authenticate_user!, only: [:index , :show]
   def index
     @grand_parents = GrandParent.all
+  end
+    def show
+    @booking = Booking.new
+    @grand_parent = GrandParent.find(params[:id])
+  end
+
+  def new
+    @grand_parent = GrandParent.new
+  end
+
+  def create
+
+    @grand_parent = GrandParent.new(grand_parent_params)
+    @grand_parent.user = current_user
+
+
+    if @grand_parent.save
+      redirect_to grand_parent_path(@grand_parent)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def grand_parent_params
+    params.require(:grand_parent).permit(:first_name, :last_name, :description, :price, :age, :hobby, :city, :photos)
   end
 end
