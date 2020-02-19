a2f98a00314448007fce8ab168a9636102eac327
Ashley Hozé <ashleyhoze@hotmail.fr>
2020-02-19 12:30:33 +0100

    map

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,5 +1,16 @@
 require("@rails/ujs").start()
 require("@rails/activestorage").start()
 require("channels")
+require("turbolinks").start();
 
 import "bootstrap";
+import '@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css';
+
+import { initMapbox } from '../plugins/init_mapbox';
+import { initAutocomplete } from '../plugins/init_autocomplete';
+
+
+document.addEventListener('turbolinks:load', () => {
+  initMapbox();
+  initAutocomplete();
+})
